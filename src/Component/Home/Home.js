cfb3b04c5c1d2d6f9f070a7bda0c240bf8db91d9
Shihab585203 <freelancersehab1@gmail.com>
2022-09-29 22:35:28 +0600

    first commit

---
+++ src/Component/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <section className='d-flex row-cols-2 g-5'>
+            <div className='m-5'>
+                <h3>HealthFit</h3>
+            </div>
+            <div>
+                right side
+            </div>
+        </section>
+    );
+};
+
+export default Home;
