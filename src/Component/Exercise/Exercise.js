f6c23ce8e193ef83890819998c72215f4dc2c7fe
Shihab585203 <freelancersehab1@gmail.com>
2022-09-30 10:11:16 +0600

    add a break section has been done

--- src/Component/Exercise/Exercise.js
+++ src/Component/Exercise/Exercise.js
@@ -1,34 +1,22 @@
-import React from 'react';
-import './Exercise.css'
+import React from "react";
+import "./Exercise.css";
 
 const Exercise = () => {
-    return (
-        <div>
-           <h3 className='mt-4 text-white'>Add A Break</h3>
+  return (
+    <div>
+      <h3 className="mt-4 text-white">Add A Break</h3>
 
-           <div className="profile-details d-flex row-cols-3 g-4 text-white text-center p-3 me-4 mt-4 rounded-3">
-        <div className="">
-          <article className="fs-3">
-            57<span className="fs-5">kg</span>
-          </article>
-          <span>Weight</span>
-        </div>
+      <div className="profile-details exercise-time d-flex row-cols-5 g-4 text-white text-center p-4 me-4 mt-4 rounded-3">
+        <button className="btn btn-primary rounded-circle">10s</button>
+        <button className="btn btn-primary rounded-circle">20s</button>
+        <button className="btn btn-primary rounded-circle">30s</button>
+        <button className="btn btn-primary rounded-circle">40s</button>
+        <button className="btn btn-primary rounded-circle">50s</button>
+      </div>
 
-        <div className="">
-          <article className="fs-3">6.3</article>
-          <span>Height</span>
-        </div>
-
-        <div className="">
-          <article className="fs-3">
-            35
-            <span className="fs-5">Yrs</span>
-          </article>
-          <span>Age</span>
-        </div>
-      </div> 
-        </div>
-    );
+      
+    </div>
+  );
 };
 
 export default Exercise;
