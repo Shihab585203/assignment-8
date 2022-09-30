a5f4c19a6f3d7c35b1afcf124542f5eafa6a5594
Shihab585203 <freelancersehab1@gmail.com>
2022-09-30 10:23:47 +0600

    exercise details added

--- src/Component/Exercise/Exercise.js
+++ src/Component/Exercise/Exercise.js
@@ -6,7 +6,7 @@ const Exercise = () => {
     <div>
       <h3 className="mt-4 text-white">Add A Break</h3>
 
-      <div className="profile-details exercise-time d-flex row-cols-5 g-4 text-white text-center p-4 me-4 mt-4 rounded-3">
+      <div className="profile-details exercise-time d-flex row-cols-5 text-white text-center p-4 me-4 mt-4 rounded-3">
         <button className="btn btn-primary rounded-circle">10s</button>
         <button className="btn btn-primary rounded-circle">20s</button>
         <button className="btn btn-primary rounded-circle">30s</button>
@@ -14,7 +14,21 @@ const Exercise = () => {
         <button className="btn btn-primary rounded-circle">50s</button>
       </div>
 
-      
+      <h3 className="mt-4 text-white">Exercise Details</h3>
+
+      <div className="profile-details exercise-time d-flex justify-content-between row-cols-5 text-white text-center p-2 me-4 mt-4 rounded-3">
+        <p>Time</p>
+        <p className="">200s</p>
+      </div>
+
+      <div className="profile-details exercise-time d-flex justify-content-between row-cols-5 text-white text-center p-2 me-4 mt-4 rounded-3">
+        <p>Break</p>
+        <p className="">15s</p>
+      </div>
+
+      <div className="row row-cols-12 me-4 ms-1 mt-4 ">
+        <button className="btn btn-primary">Activity Completed</button>
+      </div>
     </div>
   );
 };
